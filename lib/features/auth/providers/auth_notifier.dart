b2bcdd3731016d93e553604b18fa1ea024a64e68
dewanlabung <67812298+dewanlabung@users.noreply.github.com>
2026-06-28 22:55:41 +0900

    fix: restore authServiceProvider to use real dioProvider (linter corrupted it)

--- lib/features/auth/providers/auth_notifier.dart
+++ lib/features/auth/providers/auth_notifier.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' show DioException;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
